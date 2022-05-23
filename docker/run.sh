054c77a8432807a5d38e4a193daa205e60f52c6a
nabcellent <nabcellent.dev@gmail.com>
2022-05-23 13:58:40 +0300

    Dockerfile: Configuring queue worker
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 cd /home/app || exit
 
 # php artisan migrate:fresh --seed
 php artisan cache:clear
 php artisan route:cache
-php artisan serve
+php artisan serve --host=0.0.0.0 --port=8080
 
 /usr/bin/supervisord -c /etc/supervisord.conf
