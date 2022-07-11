65310d76a972dd01f9d39d41f7f49e0be86e515a
Nabcellent <nabcellent.dev@gmail.com>
2022-07-11 18:14:43 +0300

    Fixes withdrawal logic in controller
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

---
+++ app/Http/Requests/EarningRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Initiator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class EarningRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'initiator'  => ['required', new Enum(Initiator::class)],
+            'account_id' => 'required|integer',
+            'amount'     => ["required", 'numeric', 'min:20', 'max:10000']
+        ];
+    }
+}
