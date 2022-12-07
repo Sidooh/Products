fb2ea9eef2889f4638469f69796e96741f5340a9
drh <jmnabangi@gmail.com>
2022-12-07 08:38:57 +0300

    fix savings callback handler
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Http/Controllers/API/V1/SavingsController.php
+++ app/Http/Controllers/API/V1/SavingsController.php
@@ -18,13 +18,18 @@ class SavingsController extends Controller
     /**
      * @throws Throwable
      */
-    public function processCallback(Request $request)
+    public function processCallback(Request $request): \Illuminate\Http\JsonResponse
     {
         Log::info('...[CTRL - SAVINGS]: Process Savings Callback...', $request->all());
 
         $transaction = Transaction::withWhereHas('savingsTransaction', function($query) use ($request) {
             $query->whereSavingsId($request->id);
         })->whereStatus(Status::PENDING)->first();
+
+        if (!$transaction) {
+            Log::critical("Error processing savings callback - no transaction");
+            return response()->json(['status' => true]);
+        }
 
         if ($request->status === Status::FAILED->value) {
             TransactionRepository::handleFailedWithdrawal($transaction, $request);
