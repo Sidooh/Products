1dde9521f70fe992d1c4e9456b05002d6bb19a53
Nabcellent <nabcellent.dev@gmail.com>
2023-02-05 18:59:18 +0300

    CU-8669fc2tz - Update Withdrawal Flow
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/Repositories/EventRepositories/TandaEventRepository.php
+++ app/Repositories/EventRepositories/TandaEventRepository.php
@@ -63,7 +63,7 @@ class TandaEventRepository
             $tandaRequest->save();
         }
 
-        if ($transaction->status == Status::COMPLETED) {
+        if ($transaction->status == Status::COMPLETED->value) {
             SidoohNotify::notify(
                 admin_contacts(),
                 "ERROR:TANDA REQUEST\nTransaction $transaction seems to have been completed already. Confirm!!!",
@@ -73,7 +73,7 @@ class TandaEventRepository
             return;
         }
 
-        if ($transaction->status != Status::PENDING) {
+        if ($transaction->status != Status::PENDING->value) {
             SidoohNotify::notify(
                 admin_contacts(),
                 "ERROR:TANDA REQUEST\nTransaction $transaction is not pending. Confirm!!!",
@@ -196,7 +196,7 @@ class TandaEventRepository
         // Update Transaction
         $transaction = Transaction::find($tandaRequest->relation_id);
 
-        if ($transaction->status != Status::PENDING) {
+        if ($transaction->status != Status::PENDING->value) {
             SidoohNotify::notify(
                 admin_contacts(),
                 "ERROR:TANDA REQUEST\nTransaction $transaction is not pending. Confirm!!!",
