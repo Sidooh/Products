29628930e9e32e57d8dbc78dfeef8fa6bc3663de
Nabcellent <nabcellent.dev@gmail.com>
2023-02-16 20:36:34 +0300

    Decouples system balance checks
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/Http/Controllers/API/V1/ProductController.php
+++ app/Http/Controllers/API/V1/ProductController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Enums\EventType;
 use App\Helpers\AfricasTalking\AfricasTalkingApi;
+use App\Helpers\Kyanda\KyandaApi;
 use App\Helpers\Tanda\TandaApi;
 use App\Http\Controllers\Controller;
+use App\Services\SidoohNotify;
 use Illuminate\Http\JsonResponse;
 use function mb_strtolower;
 
@@ -18,11 +21,37 @@ class ProductController extends Controller
         return $this->successResponse($discounts);
     }
 
-    public function getServiceProviderBalance(): JsonResponse
+    public function checkProviderBalances(): JsonResponse
     {
+        $tanda = TandaApi::balance();
+        $kyanda = KyandaApi::balance();
+        $AT = AfricasTalkingApi::balance();
+
+        $tandaFloatBalance = $tanda[0]->balances[0]->available;
+        $kyandaFloatBalance = $kyanda['Account_Bal'];
+        $atBalance = (float) ltrim($AT['data']->UserData->balance, 'KES');
+
+        $message = "! System Balances Below Threshold:\n";
+
+        if ($tandaFloatBalance <= config('services.tanda.float.threshold')) {
+            $message .= "\t - Tanda Float: $tandaFloatBalance\n";
+        }
+        if ($kyandaFloatBalance <= config('services.kyanda.float.threshold')) {
+            $message .= "\t - Kyanda Float: $kyandaFloatBalance\n";
+        }
+        if ($atBalance <= config('services.at.airtime.threshold')) {
+            $message .= "\t - At Airtime: $atBalance\n\n";
+        }
+
+        $message .= '#SRV:Products';
+
+        SidoohNotify::notify(admin_contacts(), $message, EventType::STATUS_UPDATE);
+
         return $this->successResponse([
-            'tanda' => TandaApi::balance(),
-            'at'    => AfricasTalkingApi::balance(),
+            'balances'  => ['at' => $atBalance, 'tanda' => $tandaFloatBalance, 'kyanda' => $kyandaFloatBalance],
+            'at'        => $AT,
+            'tanda'     => $tanda,
+            'kyanda'    => $kyanda,
         ]);
     }
 }
