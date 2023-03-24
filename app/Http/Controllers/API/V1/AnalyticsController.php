9bfab1054b93010755ff3ea8f243c622cf87d8d2
Nabcellent <nabcellent.dev@gmail.com>
2023-03-24 15:28:57 +0300

    Adds Analytics
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/Http/Controllers/API/V1/AnalyticsController.php
+++ app/Http/Controllers/API/V1/AnalyticsController.php
@@ -7,84 +7,99 @@ use App\Enums\TransactionType;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class AnalyticsController extends Controller
 {
     public function sla(): JsonResponse
     {
-        $sla = Transaction::selectRaw('YEAR(created_at) as year, status, count(*) as count')
-                          ->groupByRaw('year, status')
-                          ->get();
+        $sla = Cache::remember('sla', (60 * 60), function() {
+            return Transaction::selectRaw('YEAR(created_at) as year, status, count(*) as count')
+                              ->groupByRaw('year, status')
+                              ->get();
+        });
 
         return $this->successResponse($sla);
     }
 
     public function transactions(): JsonResponse
     {
-        $data = Transaction::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
-                           ->groupBy('date', 'status')
-                           ->orderByDesc('date')
-                           ->get();
+        $data = Cache::remember('transactions', (60 * 60), function() {
+            return Transaction::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
+                              ->groupBy('date', 'status')
+                              ->orderByDesc('date')
+                              ->get();
+        });
 
         return $this->successResponse($data);
     }
 
     public function revenue(): JsonResponse
     {
-        $data = Transaction::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
-                           ->whereType(TransactionType::PAYMENT)
-                           ->whereNot('product_id', ProductType::VOUCHER)
-                           ->groupBy('date', 'status')
-                           ->orderByDesc('date')
-                           ->get();
+        $data = Cache::remember('revenue', (60 * 60), function() {
+            return Transaction::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
+                              ->whereType(TransactionType::PAYMENT)
+                              ->whereNot('product_id', ProductType::VOUCHER)
+                              ->groupBy('date', 'status')
+                              ->orderByDesc('date')
+                              ->get();
+        });
 
         return $this->successResponse($data);
     }
 
     public function transactionsByTelco(): JsonResponse
     {
-        $data = Transaction::selectRaw("destination, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
-                           ->whereProductId(ProductType::AIRTIME)
-                           ->groupBy('date', 'destination', 'status')
-                           ->orderByDesc('date')
-                           ->get()
-                           ->groupBy(fn ($tx) => getTelcoFromPhone($tx->destination));
+        $data = Cache::remember('transactionsByTelco', (60 * 60), function() {
+            return Transaction::selectRaw("destination, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
+                              ->whereProductId(ProductType::AIRTIME)
+                              ->groupBy('date', 'destination', 'status')
+                              ->orderByDesc('date')
+                              ->get()
+                              ->groupBy(fn ($tx) => getTelcoFromPhone($tx->destination));
+        });
 
         return $this->successResponse($data);
     }
 
     public function revenueByTelco(): JsonResponse
     {
-        $data = Transaction::selectRaw("destination, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
-                           ->whereProductId(ProductType::AIRTIME)
-                           ->whereType(TransactionType::PAYMENT)
-                           ->groupBy('date', 'destination', 'status')
-                           ->orderByDesc('date')
-                           ->get()
-                           ->groupBy(fn ($tx) => getTelcoFromPhone($tx->destination));
+        $data = Cache::remember('revenueByTelco', (60 * 60), function() {
+            return Transaction::selectRaw("destination, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
+                              ->whereProductId(ProductType::AIRTIME)
+                              ->whereType(TransactionType::PAYMENT)
+                              ->groupBy('date', 'destination', 'status')
+                              ->orderByDesc('date')
+                              ->get()
+                              ->groupBy(fn ($tx) => getTelcoFromPhone($tx->destination));
+        });
 
         return $this->successResponse($data);
     }
 
     public function transactionsByProduct(): JsonResponse
     {
-        $data = Transaction::selectRaw("product_id, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
-                           ->groupBy('date', 'product_id', 'status')
-                           ->orderByDesc('date')
-                           ->get()
-                           ->groupBy(fn ($tx) => ProductType::from($tx->product_id)->name);
+        $data = Cache::remember('transactionsByProduct', (60 * 60), function() {
+            return Transaction::selectRaw("product_id, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
+                              ->groupBy('date', 'product_id', 'status')
+                              ->orderByDesc('date')
+                              ->get()
+                              ->groupBy(fn ($tx) => ProductType::from($tx->product_id)->name);
+        });
 
         return $this->successResponse($data);
     }
 
     public function revenueByProduct(): JsonResponse
     {
-        $data = Transaction::selectRaw("product_id, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
-                           ->whereType(TransactionType::PAYMENT)
-                           ->groupBy('date', 'product_id', 'status')
-                           ->orderByDesc('date')
-                           ->get()
-                           ->groupBy(fn ($tx) => ProductType::from($tx->product_id)->name);
+        $data = Cache::remember('revenueByProduct', (60 * 60), function() {
+            return Transaction::selectRaw("product_id, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
+                              ->whereType(TransactionType::PAYMENT)
+                              ->groupBy('date', 'product_id', 'status')
+                              ->orderByDesc('date')
+                              ->get()
+                              ->groupBy(fn ($tx) => ProductType::from($tx->product_id)->name);
+        });
 
         return $this->successResponse($data);
     }
