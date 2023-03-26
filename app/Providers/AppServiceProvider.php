1bf71789bf03826c32aff2e31f69c1d7d59620e0
drh <jmnabangi@gmail.com>
2023-03-26 13:54:13 +0300

    adds model strictness
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,16 @@ class AppServiceProvider extends ServiceProvider
     {
         JsonResource::withoutWrapping();
 
-        // Model::preventLazyLoading(!app()->isProduction());
+        // Everything strict, all the time.
+        Model::shouldBeStrict();
+
+        // In production, merely log lazy loading violations.
+        if ($this->app->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(function($model, $relation) {
+                $class = get_class($model);
+
+                Log::warning("Attempted to lazy load [$relation] on model [$class].");
+            });
+        }
     }
 }
