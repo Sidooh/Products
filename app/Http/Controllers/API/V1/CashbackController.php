df0f6abcfe11a6ab62245d881dc15955cd78c659
drh <jmnabangi@gmail.com>
2023-04-02 17:09:29 +0300

    updates earnings investments to cater for merchant savings
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Http/Controllers/API/V1/CashbackController.php
+++ app/Http/Controllers/API/V1/CashbackController.php
@@ -63,16 +63,29 @@ class CashbackController extends Controller
 
         $date = $request->filled('date') ? Carbon::createFromFormat('d-m-Y', $request->input('date')) : new Carbon;
 
-        $builder = Cashback::selectRaw('SUM(amount) as amount, account_id')->whereNotNull('account_id')->whereNot(
-            'status',
-            Status::COMPLETED
-        )->whereDate('created_at', $date->format('Y-m-d'))->groupBy('account_id');
+        $builder = Cashback::select('cashbacks.account_id')
+            ->selectRaw("
+                    SUM(CASE
+                            WHEN t.product_id != 6 THEN cashbacks.amount
+                        END) as amount,
+                    SUM(CASE
+                        WHEN t.product_id = 6 THEN cashbacks.amount
+                    END) as merchant_amount
+                    "
+            )
+            ->leftJoin('transactions as t', 't.id', '=', 'cashbacks.transaction_id')
+            ->whereNotNull('cashbacks.account_id')
+            ->whereNot('cashbacks.status', Status::COMPLETED)
+            ->whereDate('cashbacks.created_at', $date->format('Y-m-d'))
+            ->groupBy('cashbacks.account_id');
 
-        $savings = $builder->get()->map(fn (Cashback $cashback) => [
-            'account_id'     => $cashback->account_id,
-            'current_amount' => round($cashback->amount * .2, 4),
-            'locked_amount'  => round($cashback->amount * .8, 4),
+        $savings = $builder->get()->map(fn(Cashback $cashback) => [
+            'account_id'      => $cashback->account_id,
+            'current_amount'  => round($cashback->amount * .2, 4),
+            'locked_amount'   => round($cashback->amount * .8, 4),
+            'merchant_amount' => round($cashback->merchant_amount, 4),
         ]);
+
 
         $message = "STATUS:SAVINGS\n\n";
 
@@ -84,12 +97,12 @@ class CashbackController extends Controller
                 $failed = $responses['failed'];
 
                 if (count($completed) > 0) {
-                    $message .= 'Processed earnings for '.count($completed)."  accounts\n";
+                    $message .= 'Processed earnings for ' . count($completed) . "  accounts\n";
 
                     $builder->whereIn('account_id', array_keys($completed))->update(['status' => Status::COMPLETED]);
                 }
                 if (count($failed) > 0) {
-                    $message .= 'Failed for '.count($failed).' accounts';
+                    $message .= 'Failed for ' . count($failed) . ' accounts';
 
                     $builder->whereIn('account_id', array_keys($failed))->update(['status' => Status::FAILED]);
                 }
