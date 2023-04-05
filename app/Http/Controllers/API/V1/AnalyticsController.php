660304f22bae78268758abd03686f2c176a25881
Nabcellent <nabcellent.dev@gmail.com>
2023-04-05 10:20:19 +0300

    Updates analytics cache times
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/Http/Controllers/API/V1/AnalyticsController.php
+++ app/Http/Controllers/API/V1/AnalyticsController.php
@@ -14,7 +14,7 @@ class AnalyticsController extends Controller
 {
     public function sla(): JsonResponse
     {
-        $sla = Cache::remember('sla', (60 * 60), function() {
+        $sla = Cache::remember('sla', (3600 * 24 * 7), function() {
             return Transaction::selectRaw('YEAR(created_at) as year, status, count(*) as count')
                               ->whereIn('status', [Status::COMPLETED, Status::FAILED, Status::REFUNDED])
                               ->groupByRaw('year, status')
@@ -26,7 +26,7 @@ class AnalyticsController extends Controller
 
     public function transactions(): JsonResponse
     {
-        $data = Cache::remember('transactions', (60 * 60), function() {
+        $data = Cache::remember('transactions', (3600 * 24), function() {
             return Transaction::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
                               ->groupBy('date', 'status')
                               ->orderByDesc('date')
@@ -38,7 +38,7 @@ class AnalyticsController extends Controller
 
     public function revenue(): JsonResponse
     {
-        $data = Cache::remember('revenue', (60 * 60), function() {
+        $data = Cache::remember('revenue', (3600 * 24), function() {
             return Transaction::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
                               ->whereType(TransactionType::PAYMENT)
                               ->whereNot('product_id', ProductType::VOUCHER)
@@ -52,7 +52,7 @@ class AnalyticsController extends Controller
 
     public function transactionsByTelco(): JsonResponse
     {
-        $data = Cache::remember('transactionsByTelco', (60 * 60), function() {
+        $data = Cache::remember('transactionsByTelco', (3600 * 24), function() {
             return Transaction::selectRaw("destination, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
                               ->whereProductId(ProductType::AIRTIME)
                               ->groupBy('date', 'destination', 'status')
@@ -66,7 +66,7 @@ class AnalyticsController extends Controller
 
     public function revenueByTelco(): JsonResponse
     {
-        $data = Cache::remember('revenueByTelco', (60 * 60), function() {
+        $data = Cache::remember('revenueByTelco', (3600 * 24), function() {
             return Transaction::selectRaw("destination, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
                               ->whereProductId(ProductType::AIRTIME)
                               ->whereType(TransactionType::PAYMENT)
@@ -81,7 +81,7 @@ class AnalyticsController extends Controller
 
     public function transactionsByProduct(): JsonResponse
     {
-        $data = Cache::remember('transactionsByProduct', (60 * 60), function() {
+        $data = Cache::remember('transactionsByProduct', (3600 * 24), function() {
             return Transaction::selectRaw("product_id, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count")
                               ->groupBy('date', 'product_id', 'status')
                               ->orderByDesc('date')
@@ -94,7 +94,7 @@ class AnalyticsController extends Controller
 
     public function revenueByProduct(): JsonResponse
     {
-        $data = Cache::remember('revenueByProduct', (60 * 60), function() {
+        $data = Cache::remember('revenueByProduct', (3600 * 24), function() {
             return Transaction::selectRaw("product_id, status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, SUM(amount) as amount")
                               ->whereType(TransactionType::PAYMENT)
                               ->groupBy('date', 'product_id', 'status')
